8ebd6538aff7e814521447637f9e924e7a9cc3a8
Gyarmati Tamás <tamasgy56@gmail.com>
2025-04-09 10:01:55 +0200

    Szoba méret mentés gomb

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -64,3 +64,60 @@ function AlertWrite(x, y) {
     x.innerHTML = y;
     x.classList.remove("d-none");
 }
+
+// Szoba méret mentés gomb
+saveBtn.addEventListener("click", function() {
+    const newRoomWidth = parseFloat(roomWidthInput.value);
+    const newRoomHeight = parseFloat(roomHeightInput.value);
+    const newRoomArea = (newRoomHeight * newRoomWidth) / 10000;
+    const invalidItem = items.some(item =>
+        item.itemWidth > newRoomWidth || item.itemHeight > newRoomHeight    
+    );
+    let sumArea = (items.reduce((sum, element) => sum + element.itemArea, 0));
+    dangerAlert.classList.add("d-none");
+
+    // Hibaüzenetek
+    if (newRoomWidth > 1000 || newRoomHeight > 1000) {
+        AlertWrite(dangerAlert, "<strong>Maximum szoba méret: 1000x1000!</strong>");
+        return;
+    }
+    if (!newRoomWidth || !newRoomHeight || isNaN(newRoomWidth) || isNaN(newRoomHeight)) {
+        AlertWrite(dangerAlert, "<strong>Adj meg érvényes szobaméretet!</strong>");
+        return;
+    }
+    if (sumArea > newRoomArea) {
+        AlertWrite(dangerAlert, "<strong>A hozzáadott tárgyak mérete nagyobb mint az új szoba mérete!</strong>");
+        return;
+    }
+    if (invalidItem) {
+        AlertWrite(dangerAlert, "<strong>A szoba szélessége/magassága kevesebb mint egy adott tárgyé!</strong>");
+        return;
+    }
+
+    // Mentés gomb 'PUT' request
+    fetch(apiUrlRoom, {
+        method: "PUT",
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify({
+            id: 1,
+            width: newRoomWidth,
+            height: newRoomHeight
+        })
+    })
+    .then(resp => {
+        console.log("Response: ", resp);
+    })
+    .catch(error => console.log(error));
+
+    // Az új értékeket elmentjük a tömbbe
+    roomData.width = newRoomWidth;
+    roomData.height = newRoomHeight;
+    roomData.area = newRoomArea;
+
+    // Szoba méret kiírása
+    roomSizePtag.textContent = "Szoba mérete: " + roomData.width + "x" + roomData.height + "cm" + " (" + roomData.area + "m²)" ;
+
+    // Input mezők nullázása (ez miatt kellett a 'roomData')
+    roomWidthInput.value = "";
+    roomHeightInput.value = "";
+});
